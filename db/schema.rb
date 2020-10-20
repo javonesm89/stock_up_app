5f74c979f2b577384de6a79a8628de29950698a4
javonesm89 <javosmith12@gmail.com>
2020-10-20 15:46:26 -0400

    db creation
    adding tables to db

---
+++ db/schema.rb
@@ -0,0 +1,32 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_10_20_194142) do
+
+  create_table "investors", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.integer "account_balance"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "order_name"
+    t.integer "num_of_shares"
+  end
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "name"
+    t.string "symbol"
+    t.integer "stock_price"
+  end
+
+end
