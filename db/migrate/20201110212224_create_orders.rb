213360f897bb461b8258a4e15c91e5369b85c698
javonesm89 <javosmith12@gmail.com>
2020-11-12 16:41:01 -0500

    gitignore is now working

--- db/migrate/20201110212224_create_orders.rb
+++
@@ -1,9 +0,0 @@
-class CreateOrders < ActiveRecord::Migration[5.2]
-  def change
-    create_table :orders do |g|
-      g.string :order_name
-      g.integer :investor_id
-      g.timestamps
-    end
-  end
-end
